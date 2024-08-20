3c66fed6c151d12b52358b432546b3ce6ce32135
Abhishek Bhatt <abhishekbhatti625@gmail.com>
2024-08-20 10:41:40 +0545

    index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log(typeof (4 > 1) );
+// console.log(typeof true);
+// console.log(5 == 5);
+// console.log(4 <=3 );
+console.log("checking if you can drive");
+let age = 14;
+if (21>18) {
+  console.log("you can drive");
+}
+if (age <18) {
+  console.log("Use a Bicycle");
+}
